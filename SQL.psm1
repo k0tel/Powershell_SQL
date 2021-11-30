0b722ea139c125e0601aa76684a90f9245fdf00f
Matěj Kotlín <matej.kotlin@seznam.cz>
2021-11-30 08:28:44 +0100

    Adding SQL and log modules.
    :!q

---
+++ SQL.psm1
@@ -0,0 +1,383 @@
+﻿#  VERSION : 1.23
+#  AUTOR   : Matěj Kotlín  
+#  UPDATED : 11.05.2016
+
+#- PRIVATE SECTION -----------------------------------------------------------------------------------------------
+
+$noConnections      = 'Please fill the Connection string or SQLConnection!'
+$noConnectionString = 'Please fill the Connection string!'
+$noCommand          = 'Please fill the Command!'
+$noTableName        = 'Please fill the neme of table!'
+$noCommandType      = 'Please fill the CommandType parameter!'
+$noCommandText      = 'Please fill the CommandText parameter!'
+$noProcedureName    = 'Please fill the neme of procedure!'
+$noQuery            = 'Please fill the query!'
+$noDatabaseName     = 'Please fill the neme of database!'
+$noFullFilePath     = 'Please fill the location of the backup file!'
+$noSourceTableName  = 'Please fill the neme of source table!'
+$noTargetTableName  = 'Please fill the neme of target table!'
+
+
+filter Add-Property($Name, $Value) {
+    Add-Member -type NoteProperty -name $Name -value $Value -InputObject $_ -PassThru
+}
+
+function Add-SQLCommandParameter {
+    Param (
+        [System.Data.SqlClient.SqlCommand] $Command = $(throw $noCommand),
+        [Hashtable]                        $Params  = @{}
+    )
+
+    foreach($p in $Params.Keys)
+    {
+        $Command.Parameters.AddWithValue('@' + $p, $Params[$p]) | Out-Null 
+        $paramString += $paramDelim + '@' + $p + '=' + $Params[$p]
+        $paramDelim = ', '
+    }
+    
+    Write-Verbose "SQL Command: Adding params $paramString"
+
+    return $Command
+}
+
+
+#-----------------------------------------------------------------------------------------------------------------
+
+#- PUBLIC SECTION ------------------------------------------------------------------------------------------------
+
+function Inovke-Error {
+    Param (
+        [string] $message 
+    )
+
+    Write-Output "ERROR: $message"
+
+    throw "ERROR: $message"
+}
+
+<#
+.SYNOPSIS
+    Otevře SQL connection dle zadaneho connection stringu
+#>
+function Invoke-SQLStartConnection {
+     Param (
+        [String] $ConnectionString = $(throw $noConnectionString)
+    )
+    
+    Write-Verbose 'SQL Connection: Creating connection'
+
+    try
+    {
+        $sql = New-Object Data.SqlClient.SqlConnection
+        $sql.ConnectionString = $ConnectionString
+        $sql.Open() | Out-Null
+
+        return $sql
+    }
+    catch [System.Exception] {
+        Inovke-Error -message $_
+    }
+}
+
+
+<#
+.SYNOPSIS
+    Zavře SQL connection
+#>
+function Invoke-SQLEndConnection {
+     Param (
+        [Data.SqlClient.SqlConnection] $SQLConnection = $(throw $noConnectionString)
+    )
+    
+    Write-Verbose 'SQL Connection: Disposing connection'
+
+    if ($SQLConnection)
+    {
+        $SQLConnection.Dispose() | Out-Null
+    }
+}
+
+
+<#
+.SYNOPSIS
+    Vytvoří nový SQL Command
+    - Nejprve se vytvori SQL Connection dle zadaneho Connection stringu respektive SQLConnection
+    - Pote se vytvori SQL Command se zadanyma parametrama
+#>
+function Invoke-SQLStartCommand {
+     Param (
+        # pri zadani SQLConnection se pocita s tim ze SQL je pripojeno
+        [Data.SqlClient.SqlConnection] $SQLConnection,
+
+        # pri zadani ConnectionStringu si funkce vytvori svoje pripojeni 
+        [String]                       $ConnectionString,
+        [string]                       $CommandType = $(throw $noCommandType),
+        [int]                          $CommandTimeout = 30,
+        [string]                       $CommandText = $(throw $noCommandText)
+    )
+    
+    Write-Verbose "SQL Command: Creating new command"
+
+    if ($ConnectionString) 
+    { 
+        $SQLConnection = Invoke-SQLStartConnection -ConnectionString $ConnectionString
+    }
+    elseif (-not $SQLConnection) 
+    {
+        throw $noConnections
+    }
+
+    try
+    {
+        $cmd = New-Object System.Data.SqlClient.SqlCommand
+        $cmd.Connection = $SQLConnection
+        $cmd = $SQLConnection.CreateCommand()
+    
+        $cmd.Parameters.Clear()
+    
+        $cmd.CommandType = $CommandType 
+        $cmd.CommandTimeout = $CommandTimeout
+        $cmd.CommandText = $CommandText
+        
+        return $cmd
+    }
+    catch [System.Exception] {
+        Inovke-Error -message $_
+    }
+}
+
+
+<#
+.SYNOPSIS
+    Zavře SQL Command a dle prepinace $CloseSQLConnection bud uzavre SQL Connection nebo nikoliv
+#>
+function Invoke-SQLEndCommand {
+     Param (
+        [System.Data.SqlClient.SqlCommand] $Command            = $(throw $noCommand),
+        [bool]                             $CloseSQLConnection = $false
+    )
+
+    Write-Verbose "SQL Command: Disposing"
+    
+    if ($Command)
+    {
+        $Command.Dispose() | Out-Null
+    }
+
+    if ($CloseSQLConnection)
+    {
+        Invoke-SQLEndConnection -SQLConnection $Command.Connection
+    }
+}
+
+
+<#
+.SYNOPSIS
+    Zavolani stored procedure na DB 
+    - Zalozi SQL Connection spolu s SQL Commandem a po zavolani procedury Command i Connection uzavre podle toho jestli je to zadane
+#>
+function Invoke-SQLStoredProcedure {
+     Param (
+        # pri zadani SQLConnection se pocita s tim ze SQL je pripojeno
+        [Data.SqlClient.SqlConnection] $SQLConnection,
+
+        [String]    $ConnectionString,
+        [String]    $ProcedureName      = $(throw $noProcedureName),
+        [Hashtable] $Params             = @{},
+        [bool]      $CloseSQLConnection = $true
+    )
+    
+    try 
+    {
+        if ($ConnectionString) 
+        {
+            $SQLConnection = Invoke-SQLStartConnection -ConnectionString $ConnectionString
+        }
+        elseif (-not $SQLConnection) 
+        {
+            throw $noConnections
+        }
+
+        $cmd = Invoke-SQLStartCommand -SQLConnection $SQLConnection -CommandType 'StoredProcedure' -CommandText $ProcedureName
+            
+        Add-SQLCommandParameter -Command $cmd -Params $Params
+        
+        Write-Verbose "SQL Stored procedure: Executing procedure $ProcedureName"
+        
+        $result = $cmd.ExecuteReader()
+
+        $table = New-Object System.Data.DataTable
+        $table.Load($result)
+
+        return $table
+    }
+    catch [System.Exception] {
+        Inovke-Error -message $_
+    }
+    finally {
+        Invoke-SQLEndCommand -Command $cmd -CloseSQLConnection $CloseSQLConnection
+
+        $result.Dispose()
+    }
+}
+
+
+<#
+.SYNOPSIS
+    Zavolani jednoho query do DB
+    - Zalozi SQL Connection spolu s SQL Commandem a po zavolani procedury Command i Connection uzavre
+#>
+function Invoke-SQLQuery {
+    Param ( 
+        # pri zadani SQLConnection se pocita s tim ze SQL je pripojeno
+        [Data.SqlClient.SqlConnection] $SQLConnection,
+
+		[string]    $ConnectionString,
+        [string]    $Query              = $(throw $noQuery),
+        [Hashtable] $Params             = @{},
+        [bool]      $CloseSQLConnection = $true
+	) 
+    
+    try
+    {
+        if ($ConnectionString) 
+        {
+            $SQLConnection = Invoke-SQLStartConnection -ConnectionString $ConnectionString
+        }
+        elseif (-not $SQLConnection) 
+        {
+            throw $noConnections
+        }
+
+        $cmd = Invoke-SQLStartCommand -SQLConnection $SQLConnection -CommandType 'Text' -CommandText $Query
+
+        # pokud existuji nejake parametry
+        if ($Params) 
+        {
+            Add-SQLCommandParameter -Command $cmd -Params $Params
+        }
+    
+        Write-Verbose "SQL Query: $Query"
+
+        $result = $cmd.ExecuteReader()
+        
+        $table = New-Object System.Data.DataTable
+        $table.Load($result)
+
+        return $table
+    }
+    catch [System.Exception] {
+        Inovke-Error -message $_
+    }
+    finally {
+        Invoke-SQLEndCommand -Command $cmd -CloseSQLConnection $CloseSQLConnection
+
+        $result.Dispose()
+    }
+}
+
+
+<#
+.SYNOPSIS
+    Funkce dropne tabulku z DB
+#>
+function Invoke-SQLDropTable {
+    Param (
+        [string] $ConnectionString   = $(throw $noConnectionString),
+        [string] $TableName          = $(throw $noTableName),
+        [bool]   $CloseSQLConnection = $true
+    )
+    
+    Write-Verbose "SQL Drop table: $TableName"
+    
+    $cmd = Invoke-SQLStartCommand -ConnectionString $ConnectionString -CommandType "Text" -CommandText "DROP TABLE $TableName"
+    
+    $cmd.ExecuteNonQuery() | Out-Null
+    
+    Invoke-SQLEndCommand -Command $cmd -CloseSQLConnection $CloseSQLConnection
+}
+
+
+<#
+.SYNOPSIS
+    Funkce kompletne vymaze obsah tabulky
+#>
+function Invoke-SQLTruncateTable {
+    Param (
+        [string] $ConnectionString   = $(throw $noConnectionString),
+        [string] $TableName          = $(throw $noTableName),
+        [bool]   $CloseSQLConnection = $true
+    )
+    
+    Write-Verbose "SQL Truncate table: $TableName"
+    
+    $cmd = Invoke-SQLStartCommand -ConnectionString $ConnectionString -CommandType "Text" -CommandText "TRUNCATE TABLE $TableName"
+    
+    $cmd.ExecuteNonQuery() | Out-Null
+    
+    Invoke-SQLEndCommand -Command $cmd -CloseSQLConnection $CloseSQLConnection
+}
+
+
+<#
+.SYNOPSIS
+    Funkce provede Full backup databaze na zadane umisteni
+#>
+function Invoke-SQLBackupDatabaseFull {
+    Param (
+        [string] $ConnectionString   = $(throw $noConnectionString),
+        [string] $DatabaseName       = $(throw $noDatabaseName),
+        [string] $FullFilePath       = $(throw $noFullFilePath),
+        [bool]   $CloseSQLConnection = $true
+    )
+    
+    Write-Verbose "SQL Backup Database: $DatabaseName to file: $FullFilePath"
+    
+    $cmd = Invoke-SQLStartCommand -ConnectionString $ConnectionString -CommandType "Text" -CommandText "BACKUP DATABASE $DatabaseName TO DISK = '$FullFilePath' WITH FORMAT, MEDIANAME = '$DatabaseName', NAME = 'Full Backup of $DatabaseName';"
+    
+    $cmd.ExecuteNonQuery() | Out-Null
+    
+    Invoke-SQLEndCommand -Command $cmd -CloseSQLConnection $CloseSQLConnection
+}
+
+
+<#
+.SYNOPSIS
+    Funkce dle zadaneho jména tabulky provede kompletní kopii
+#>
+function Invoke-SQLCopyTable {
+    Param (
+        [string] $ConnectionString          = $(throw $noConnectionString),
+        [string] $SourceTableName           = $(throw $noSourceTableName),
+        [string] $TargetTableName           = $(throw $noTargetTableName),
+        [string] $DeleteIfExistTargetTable  = $true,
+        [bool]   $CloseSQLConnection        = $true
+    )
+    
+    Write-Verbose "SQL Copy table: $SourceTableName to table: $TargetTableName"
+    
+    if ($DeleteIfExistTargetTable) {
+        $deleteTable = "IF OBJECT_ID('$TargetTableName') IS NOT NULL DROP TABLE $TargetTableName;"
+    } else { $deleteTable = "" }
+
+    $cmd = Invoke-SQLStartCommand -ConnectionString $ConnectionString -CommandType "Text" -CommandText "$deleteTable SELECT * INTO $TargetTableName FROM $SourceTableName;"
+    
+    $cmd.ExecuteNonQuery() | Out-Null
+
+    Invoke-SQLEndCommand -Command $cmd -CloseSQLConnection $CloseSQLConnection
+}
+
+#-----------------------------------------------------------------------------------------------------------------
+
+Export-ModuleMember -Function @(
+    'Invoke-SQLStartConnection'
+    'Invoke-SQLEndConnection'
+    'Invoke-SQLStartCommand'
+    'Invoke-SQLEndCommand'
+    'Invoke-SQLStoredProcedure'
+    'Invoke-SQLQuery'
+    'Invoke-SQLDropTable'
+    'Invoke-SQLTruncateTable'
+    'Invoke-SQLBackupDatabaseFull'
+    'Invoke-SQLCopyTable'
+)
---
+++ log.psm1
@@ -0,0 +1,132 @@
+﻿#  VERSION : 1.03
+#  AUTOR   : Matěj Kotlín  
+#  CREATED : 26.06.2016
+#  UPDATE  : - add username into log message
+
+#- PRIVATE SECTION ---------------------------------------------------------------------------------------------
+
+# cache variable for global log data
+$global:content = ""
+
+function LogMsg {
+    Param (
+        [string] $Type,
+        [string] $Text = "",
+        [string] $User
+    )
+
+    $dateTime = Get-Date -f "dd.MM.yyyy HH:mm:ss"
+    
+    # if is missing parameter of new line it will be switched to default blank space
+    if ($newLineCharacter.Length -eq 0) {
+        $newLineCharacter = " "
+    }
+
+    # replacing new line in text message
+    $text = $text.replace("`n", $newLineCharacter).replace("`r", $newLineCharacter)
+
+    # add log message separator
+    if ($messageSeparator.Length -gt 0) { 
+        $messageSeparator = "`r`n$messageSeparator" 
+    }
+    
+    return "$dateTime;$Type;$User;$Text$messageSeparator"
+}
+
+#- PUBLIC VISIBLE SECTION ----------------------------------------------------------------------------------------
+
+<#
+    .Synopsis
+        Log specific message with specific setting to file or Event viewer.
+
+    .Example
+        Log -Type "ERROR" -Message "error on line 10"
+
+    .Example
+        Log -Type "ERROR" -Message "error on line 10" -Verbose
+
+    .Example
+        Log -Message "added 10 accounts" -FilePath "C:\temp\logfile.log"
+
+    .Example
+        Log -Type "INFO" -Message "added 10 accounts" -LogEventVwr "SpecLog01"
+    
+    .Example
+        Log -Type "INFO" -Message "added 10 accounts" -User "jnovak"
+
+#>
+function Log {
+    [cmdletbinding()]
+    Param (
+        [ValidateSet("FATAL", "ERROR", "WARN", "INFO", "DEBUG")] 
+        [string] $Type = "INFO",
+        [string] $User = [Environment]::UserName,
+        [Parameter(Mandatory=$true)]        
+        [string] $Message,
+        [string] $FilePath,
+        [string] $LogEventVwr,
+
+        # if the message contains special characters `n or `r will be switched to this character in the message for better output
+        [string] $NewLineCharacter = "",
+        # line separator of the one message
+        [string] $MessageSeparator = ""
+    )
+
+    Process {
+        try {
+            # separator of local variable message (content)
+            if ($global:content.Length -eq 0) { $sep = "" } else { $sep = "`r`n"}
+        
+            $content = $global:content
+
+            $global:content = $(LogMsg $Type $Message $User)
+
+            Write-Verbose $global:content
+
+            $global:content += "$sep$content"
+
+            # if filepath param is filled save the message into this file and clear global content message cache
+            if ($FilePath) { 
+                if (Test-Path $FilePath) { 
+                    $contentLog = Get-Content $FilePath
+                } 
+                else { 
+                    Write-Verbose "FilePath $FilePath doesn´t exist!" 
+                }
+
+                $global:content > $FilePath
+                $contentLog >> $FilePath 
+
+                $global:content = ""
+            }
+
+            # if logEventViewer is filled save the message into this event viewer name and clear global content message cache
+            if ($LogEventVwr) {
+                # check if event Log exists and if no exist so create it
+                if (![System.Diagnostics.EventLog]::SourceExists($LogEventVwr)) {  
+                    [System.Diagnostics.EventLog]::CreateEventSource($LogEventVwr, $LogEventVwr)
+
+                    Write-Verbose "Creating new logName: $LogEventVwr"
+                }  
+
+                # write message into log
+                $mylog = New-Object System.Diagnostics.Eventlog  
+                $mylog.Source = $LogEventVwr
+                $mylog.WriteEntry($global:content)
+
+                $global:content = ""
+            }
+        }
+        catch [System.Exception] {
+            Write-Host "ERROR: $_"
+
+            return $error.Count
+        }
+    }
+}
+
+#-----------------------------------------------------------------------------------------------------------------
+
+Export-ModuleMember -Function @(
+    'Log'
+)
